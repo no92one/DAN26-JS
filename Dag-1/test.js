3210146dbea043a09445a06b5df8786932f5ce8a
unknown <linus.lindroth.92@gmail.com>
2026-09-08 10:54:44 +0200

    moved file to filder 'Dag-1'

---
+++ Dag-1/test.js
@@ -0,0 +1,10 @@
+let resturantName = "Linus meal dealer"
+
+console.log(resturantName)
+console.log("Välkommen till " + resturantName)
+console.log(resturantName + " har öppet till 22:00")
+
+resturantName = "Håkan's Meal Deal"
+
+console.log("Vi kommer imorgon bytta namn till " + resturantName)
+

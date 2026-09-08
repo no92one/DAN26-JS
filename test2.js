1508968323b5c1c32c0779340949385699094344
unknown <linus.lindroth.92@gmail.com>
2026-09-08 10:53:40 +0200

    initial commit

---
+++ test2.js
@@ -0,0 +1,13 @@
+const productName = "Margherita"
+const price = 129
+const price2 = 129.99
+const isAvailable = true
+const condition = price > price2
+const condition2 = price < price2
+
+console.log(typeof productName)
+console.log(typeof price)
+console.log(typeof price2)
+console.log(typeof isAvailable)
+console.log(typeof condition)
+console.log(typeof condition2)

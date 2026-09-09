9c74745b4b34e914a9965be589403ef5e5932997
no92one <linus.lindroth.92@gmail.com>
2026-09-09 15:32:30 +0200

    how to log on same line

---
+++ Dag-2/log-same-line.js
@@ -0,0 +1,10 @@
+const menu = ["Margherita", "Vesuvio", "Hawaii", 
+    "Vesuvio", "Hawaii"]
+
+let result = "";
+
+for (let i = 0; i < menu.length; i++) {
+    result = result + menu[i] + " "
+}
+
+console.log(result)

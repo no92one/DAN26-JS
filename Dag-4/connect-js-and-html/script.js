4cf9a43aa00c7d729882a04c989fd1fa8f184b3e
unknown <linus.lindroth.92@gmail.com>
2026-09-15 11:50:23 +0200

    js is now building html-element from a list och products-objects

--- Dag-4/connect-js-and-html/script.js
+++ Dag-4/connect-js-and-html/script.js
@@ -1,15 +1,77 @@
-console.log("Script filen är kopplad!!!");
+// console.log("Script filen är kopplad!!!");
 
-const title = document.querySelector("h1");
+// const title = document.querySelector("h1");
+// console.log(title);
 
-console.dir(title);
-console.log(title.textContent);
+// console.dir(title);
+// console.log(title.textContent);
 
-title.textContent = "Jag har ändrat texten nu i javascript!";
+// title.textContent = "Jag har ändrat texten nu i javascript!";
 
-console.log(title.classList);
+// console.log(title.classList);
 
-title.classList.remove("red");
-title.classList.add("blue");
+// title.classList.remove("red");
+// title.classList.add("blue");
 
-console.log(title.classList);
+// console.log(title.classList);
+
+//--------------------------------------------------------------------
+
+// const products = ["Margarita", "Vesuvio", "Kebabpizza"];
+
+// const productsElements = document.querySelectorAll("h2");
+
+// console.log(productsElements);
+
+// for (let i = 0; i < productsElements.length; i++) {
+//   productsElements[i].textContent = products[i];
+// }
+
+//--------------------------------------------------------------------
+
+const products = [
+  {
+    "name": "Margarita",
+    "price": 80
+  },
+  {
+    "name": "Vesuvio",
+    "price": 85
+  },
+  {
+    "name": "Kebabpizza",
+    "price": 120
+  },
+  {
+    "name": "Hawaii",
+    "price": 100
+  },
+  {
+    "name": "Fungi",
+    "price": 100
+  },
+  {
+    "name": "Kyckingpizza",
+    "price": 110
+  },
+  {
+    "name": "Calzone",
+    "price": 90
+  }
+];
+
+const menuElement = document.querySelector("#menu");
+
+console.dir(menuElement);
+
+for (let i = 0; i < products.length; i++) {
+  const product = document.createElement("article");
+
+  product.classList.add("product");
+
+  console.log("products - ", product);
+
+  product.innerHTML = `<h2>${products[i].name}</h2>
+  <p>${products[i].price} kr</p>`;
+  menuElement.appendChild(product);
+}

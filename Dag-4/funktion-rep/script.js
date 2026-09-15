ad909590d0a6451d92dbaf8e8e8ea9c724579a54
unknown <linus.lindroth.92@gmail.com>
2026-09-15 10:14:32 +0200

    added a feunction example

---
+++ Dag-4/funktion-rep/script.js
@@ -0,0 +1,42 @@
+import { input } from "@inquirer/prompts";
+
+// const name = await input({ "message": "Skriv vad du heter:" });
+// console.log(`Hej ${name}!`);
+
+let run = true;
+
+while (run) {
+  console.log("\n\n");
+  console.log("1. addera");
+  console.log("2. sub");
+  console.log("3. multi");
+  console.log("4. avsluta");
+  const choice = await input({ "message": "Välj ett av meny valen 1-4:" });
+
+  if (choice == "1") {
+    const number1 = Number.parseInt(await input({ "message": "Skriv in tal1:" }));
+    const number2 = Number.parseInt(await input({ "message": "Skriv in tal2:" }));
+    const result = number1 + number2;
+    console.log(`${number1} + ${number2} = ${result}`);
+  } else if (choice == "2") {
+    const number1 = Number.parseInt(await input({ "message": "Skriv in tal1:" }));
+    const number2 = Number.parseInt(await input({ "message": "Skriv in tal2:" }));
+    const result = number1 - number2;
+    console.log(`${number1} - ${number2} = ${result}`);
+  } else if (choice == "3") {
+    const number1 = Number.parseInt(await input({ "message": "Skriv in tal1:" }));
+    const number2 = Number.parseInt(await input({ "message": "Skriv in tal2:" }));
+    const result = number1 * number2;
+    console.log(`${number1} * ${number2} = ${result}`);
+  } else if (choice == "4") {
+    run = false;
+    console.log("Programmet avslutas");
+  } else {
+    console.log("Du måste välja mellan 1-4!");
+  }
+
+}
+
+
+
+
